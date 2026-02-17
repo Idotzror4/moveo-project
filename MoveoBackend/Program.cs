84c6721ea4207d4cc430b28ad84e568049c59a87
Idotzror4 <idotzror4@gmail.com>
2026-02-17 17:19:03 +0200

    Add automatic database migration on startup

--- MoveoBackend/Program.cs
+++ MoveoBackend/Program.cs
@@ -48,6 +48,21 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
